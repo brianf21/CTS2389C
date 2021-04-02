623be64bd2b3a6f741ebb8faa12bcaa2cd8c6f89
brianf21 <brian.faris@gmail.com>
2021-04-01 23:54:24 -0400

    Update BasicSettings.ps1

--- BasicSettings.ps1
+++ BasicSettings.ps1
@@ -1,8 +1,9 @@
+$nic = Read-Host "Which network card? "
 $IPAddress = Read-Host "What is the IP Address? "
 $SubnetMask = Read-Host "What is the subnet mask in CIDR format? "
 $Gateway = Read-Host "What is the gateway? "
 $Host_Name = Read-Host "What is the hostname? "
 $DNSServers = Read-Host "What are the DNS Server addresses separated by commas? "
 
-New-NetIPAddress -IPAddress "$IPAddress" -PrefixLength "$SubnetMask" -DefaultGateway "$Gateway" -InterfaceAlias Ethernet | Set-DnsClientServerAddress -ServerAddresses "$DNSServers"
+New-NetIPAddress -IPAddress "$IPAddress" -PrefixLength "$SubnetMask" -DefaultGateway "$Gateway" -InterfaceAlias $nic | Set-DnsClientServerAddress -ServerAddresses "$DNSServers"
 Rename-Computer -NewName "$Host_Name" -Restart
